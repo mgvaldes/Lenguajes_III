33dc8ebc0e38b38266eb5dc606865787af119022
fedep3 <fedep3@gmail.com>
2010-07-06 18:42:38 -0430

    Arregle la clase return

--- ASTReturn.java
+++ ASTReturn.java
@@ -3,12 +3,25 @@ import java.io.*;
 public class ASTReturn extends ASTInstruccion {
 
     private int offset;
+    private Tipo dest;
+    private ASTExpresion expr;
 
     public ASTReturn(int o) {
         super("return");
 	ireturn = true;
 	ibreak = false;
         offset = o;
+        dest = null;
+        expr = null;
+    }
+
+    public ASTReturn(int o, Tipo d, ASTExpresion e) {
+        super("return");
+	ireturn = true;
+	ibreak = false;
+        offset = o;
+        dest = d;
+        expr = e;
     }
 
     public void setOffset(int o){
